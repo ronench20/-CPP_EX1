0883d14dfce42eb531d0a2bdec37d73c1a08fc80
ronench20 <ronencher@gmail.com>
2025-03-24 20:03:22 +0200

    sec commit

--- Graph.cpp
+++ Graph.cpp
@@ -2,3 +2,37 @@
 
 #include <iostream>
 
+Graph::Graph(int vertices) {
+    numOfVertices = vertices;
+    adjList = new Node * [vertices];
+}
+
+//destructor
+Graph::~Graph(){
+    for (int i = 0; i < numOfVertices; ++i) {
+        Node curr = adjList[i];
+        if (curr == NULL){ //check why does it's not working without
+            break;
+        }
+        Node* temp = curr;
+        curr = curr.next;
+        delete temp;
+    }
+    delete[] adjList;
+}
+void Graph::addEdge(int src, int dest, int weight) {
+    if(src < 0 || src >= numOfVertices || dest < 0 || dest >= numOfVertices){
+        return;
+    }
+    Node* newNode = new Node;
+    newNode ->vertex = src;
+    newNode ->weight = weight;
+    newNode ->next = adjList[src];
+    adjList[src] = newNode;
+
+    Node* newNodeS = new Node;
+    newNodeS ->vertex = src;
+    newNodeS ->weight = weight;
+    newNodeS ->next = adjList[src];
+    adjList[src] = newNodeS;
+}
