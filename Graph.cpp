1ce9a0f6444e35423fec6b20240a4da2db8baf21
ronench20 <ronencher@gmail.com>
2025-03-25 16:50:46 +0200

    third commit

--- Graph.cpp
+++ Graph.cpp
@@ -20,8 +20,10 @@ Graph::~Graph(){
     }
     delete[] adjList;
 }
+
 void Graph::addEdge(int src, int dest, int weight) {
     if(src < 0 || src >= numOfVertices || dest < 0 || dest >= numOfVertices){
+        std::cout << "invalid Node\n"
         return;
     }
     Node* newNode = new Node;
@@ -35,4 +37,37 @@ void Graph::addEdge(int src, int dest, int weight) {
     newNodeS ->weight = weight;
     newNodeS ->next = adjList[src];
     adjList[src] = newNodeS;
+}
+
+void Graph::removeEdge(int src, int dest) {
+    if (src < 0 || src >= numOfVertices || dest < 0 || dest >= numOfVertices){
+        std::cout << "invalid Node\n"
+        return;
+    }
+    Node* curr = adjList[src];
+    Node* prev = NULL;
+    while (curr != NULL && curr -> vertex != src){
+        prev = curr;
+        curr = curr -> next;
+    }
+    if (curr != NULL){
+        if (prev == NULL){
+            adjList[dest] = curr -> next
+        } else{
+            prev ->next = curr->next;
+        }
+        delete curr;
+    }
+}
+
+void Graph::printGraph() {
+    for (int i = 0; i < numOfVertices; ++i) {
+        std::cout << "Node " << i << ":";
+        Node* curr = adjList[i];
+        while (curr != NULL){
+            std::cout << " (" << curr ->vertex << ", weight= " << curr ->weight << ")";
+            curr = curr ->next;
+        }
+        std::cout << std::endl;
+    }
 }
