fcb3e3675083c628b7e5fecd91234682d1e65962
ronench20 <ronencher@gmail.com>
2025-04-01 22:41:37 +0300

    sixth commit

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -40,3 +40,36 @@ Graph Algorithms::bfs(Graph &graph,int src){
     cout << endl;
     return graph;
 }
+
+Graph Algorithms::dfs(Graph &graph, int src) {
+    bool visited[MAX_VALUE] = {false};
+    int queue[MAX_VALUE];
+    int front = 0;
+    int rear = 0;
+
+    visited[src] = true;
+    enqueue(queue, rear, src);
+    while (!isEmpty(front, rear)){
+        int curr = dequeue(queue,front);
+        cout << curr << " ";
+
+        int children[MAX_VALUE];
+        int childrenCount = 0;
+        Node* temp = graph.adjList[curr];
+        while (temp != nullptr){
+            children[childrenCount++] = temp->vertex;
+            temp = temp->next;
+        }
+        for (int i = childrenCount - 1; i >= 0; i--) {
+            int next = children[i];
+            if (!visited[next]){
+                visited[next] = true;
+                enqueue(queue, rear, next);
+            }
+        }
+    }
+    cout << endl;
+    return graph;
+}
+
+Graph Algorithms::dijkstra(Graph &graph, int src) {}
